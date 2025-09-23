4342ff698afe8f1ecc75aa1657dcee7e644574da
Your Name <you@example.com>
2025-09-23 17:34:32 -0400

    added assignemtn

---
+++ hw/cpractice/src/mystrings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ASCII_MIN 32
+#define ASCII_MAX 126
+#define MIN_STRING_LENGTH 4
+
+int main(int argc, char *argv[]) {
+    // Your solution here!
+    return 0;
+}
